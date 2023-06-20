aaa3be6243ce466521b938da79890d7bb0f60d8d
Gele <gellehussen@gmail.com>
2023-06-20 21:37:15 +0200

    Created database with activity table

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext> (opt =>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
@@ -21,5 +28,26 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+using var Scope = app.Services.CreateScope();
+var service = Scope.ServiceProvider;
+
+try
+{
+    var context = service.GetRequiredService<DataContext>();
+
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+   var logger =  service.GetRequiredService<ILogger<Program>>();
+
+   logger.LogError(ex, "Error occored");
+ 
+  
+   
+}
+
 
 app.Run();
