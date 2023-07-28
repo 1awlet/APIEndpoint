264640e99865939b22cd5e45f254d55fae4ed1ca
Gele <gellehussen@gmail.com>
2023-07-28 03:20:36 +0200

    Code clean up, added extention folder for the services collections

---
+++ API/Extensions/ServiceExentions.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.ActivitiesProvider;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ServiceExentions
+    {
+
+        //This will refer to builder."services".AddAplicationServices 
+        //basically when you calling it inside program.cs, so you will only have to pass configuration
+        public static IServiceCollection AddAplicationServices(
+            this IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
+
+            services.AddMediatR(typeof(getList.Handler));
+
+            //services.AddAutoMapper(typeof )
+            services.AddAutoMapper(typeof(MappingFIelds).Assembly);
+
+
+return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Application.ActivitiesProvider;
 using Application.Core;
 using MediatR;
@@ -10,36 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext> (opt =>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
-
-
-
-
-
-
-
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-
-
-builder.Services.AddMediatR(typeof(getList.Handler));
-
-//builder.Services.AddAutoMapper(typeof )
-builder.Services.AddAutoMapper(typeof(MappingFIelds).Assembly);
+//We don't need to pass "Services", because this keyword recognizes we using builder."Services"
+builder.Services.AddAplicationServices(builder.Configuration);
 
 
 var app = builder.Build();
