c5de52dca8163dd5af47c67ede718d635310ac92
Gele <gellehussen@gmail.com>
2023-08-11 22:48:05 +0200

    Styled edit event component

--- activities-api/src/Components/Edit-Forms/Edit-Forms.tsx
+++ activities-api/src/Components/Edit-Forms/Edit-Forms.tsx
@@ -4,11 +4,18 @@ import "./style.css";
 const EditForm = ()=>{
 
     return(
-        <div className="editFormContainer">
-     
+        <div >
+            <form > 
+            <div className="editFormContainer">
             <input placeholder="Title"/>
             <input placeholder="Description"/>
             <input placeholder="venue"/>
+           
+           <div>  <button className="cancel">Cancel </button>
+            <button className="submitbtn" type="submit"> Submit </button>
+            </div>
+            </div>
+            </form>
         </div>
     )
 
