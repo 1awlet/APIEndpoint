87214ef9df6827683e3bfb3a7d642ca91545c946
Gele <gellehussen@gmail.com>
2023-10-06 16:42:03 +0200

    formatting, organizing and removing unused code for edit form

--- activities-api/src/Components/Edit-Forms/Edit-Forms.tsx
+++ activities-api/src/Components/Edit-Forms/Edit-Forms.tsx
@@ -1,12 +1,7 @@
-import { FormEventHandler, useEffect, useState } from "react";
-
 import "./style.css";
-import { channel } from "diagnostics_channel";
-import { ChangeEvent } from "react";
-import { FormEvent } from "react";
 import { useStore } from "../../Store/store";
 import { observer } from "mobx-react-lite";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import FormInput from "../FormInputs/formInputs";
 export type Activity= {
     id:string,
@@ -16,40 +11,10 @@ export type Activity= {
     date:string
   
   }
-type prop ={
-    activities?:Activity | undefined 
-}
 
-const EditForm = observer(({activities:selectedActivity}:prop)=>{
-    const initialState =  selectedActivity ?? {
-    id:"",
-    description: "",
-    title: "",
-    venue: "",
-    date: "",
-    
-    }
-
+const EditForm = observer(()=>{
     const {activityId} = useParams();
-    
-    console.log(activityId)
     const {activityStore}= useStore()
-    const {createActivity}=activityStore;
-    const [activity, setActivity] = useState(initialState)
-    const Navigate = useNavigate()
-
-    const getActivity:Activity | undefined = activityStore.activities.find((x:Activity)=> x.id ==activityId)
-    useEffect(()=>{
-      
-            const findActivity= activityStore.activities.find(x => x.id == activityId);
-            if(findActivity){
-                setActivity(findActivity)
-        }
-        
-    },[activityId,activityStore])
-
-
-  
     return(
         <div >
 
@@ -58,18 +23,16 @@ const EditForm = observer(({activities:selectedActivity}:prop)=>{
                    activityStore.activities.map((activity)=>{
                     if(activity.id == activityId){
                         return(
-                        
-                            <FormInput selectedActivity={activity}  key={activityId}/>
+                            <FormInput 
+                             selectedActivity={activity}  
+                             key={activityId}
+                             />
                         )
                     }
                    })
                     :
                     <FormInput />
                 }
-
-               
-            
-    
         </div>
     )
 
