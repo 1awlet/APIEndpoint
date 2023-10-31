6067d577989049c4a368cdf194b9d8ac9ce454e2
Gele <gellehussen@gmail.com>
2023-10-31 21:57:39 +0100

    added hmlt content in the home

---
+++ activities-api/src/Components/Home Page/Home.tsx
@@ -0,0 +1,16 @@
+
+
+
+const Home = ()=>{
+
+    return(
+        <div>
+            <img src="#" />
+            <h2>Welcome to activity dashbord</h2>
+            
+        </div>
+    )
+}
+
+
+export default Home;
