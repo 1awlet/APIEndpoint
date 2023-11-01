f871a156bf18b10e749c2fef536ccb195bbffc95
Gele <gellehussen@gmail.com>
2023-11-01 23:03:15 +0100

    aligned home page items center

--- activities-api/src/Components/Home Page/Home.tsx
+++ activities-api/src/Components/Home Page/Home.tsx
@@ -4,7 +4,7 @@ import "./home.css"
 const Home = ()=>{
     const Navigate = useNavigate();
     return(
-        <div>
+        <div    className="homeContainer">
             <img src="#" />
             <h2>Welcome to activity dashbord</h2>
             <button onClick={()=>Navigate("/act")}>Take me to the dashboard</button>
