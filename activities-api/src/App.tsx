8c1e0153ae9e039a66593a02b2284876163bc88e
Gele <gellehussen@gmail.com>
2023-11-04 23:49:53 +0100

    app.tsx code formatting and improvement

--- activities-api/src/App.tsx
+++ activities-api/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './Routes/Nav-Bar/navbar';
 import DashBoard from './Components/DashBoard/dashBoard';
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 import agent from './api-agent/agent';
 import LoadingSpinner from './Components/LoadingComponent/loading';
 import { useStore } from './Store/store';
@@ -16,24 +16,24 @@ import Test from './Components/test';
 import EditForm from './Components/Edit-Forms/Edit-Forms';
 import Details from './Components/DetailsView/Details';
 import Home from './Components/Home Page/Home';
-export type Activity= {
-  id:string,
-  description:string,
-  title:string,
-  venue:string,
-  date:string
+
+
+export type Activity = {
+  id: string,
+  description: string,
+  title: string,
+  venue: string,
+  date: string
 
 }
 
-
-
 function App() {
-  const {activityStore} = useStore()
+  const { activityStore } = useStore()
 
-  useEffect(()=>{
-   activityStore.fetchActivities()
+  useEffect(() => {
+    activityStore.fetchActivities()
 
-  },[activityStore])
+  }, [activityStore])
 
 
 
@@ -41,15 +41,15 @@ function App() {
     <div className="App">
 
       <Routes>
-      <Route index element={<Home />} />
-       <Route path='/' element={<Navbar />} > 
-      
-       <Route path='add/:activityId?' element={<EditForm />} />
-       <Route path='selectedActivity/:activityID' element={<Details />}/>
-       <Route path='/act' element={<DashBoard />} />
-       </Route>
+        <Route index element={<Home />} />
+        <Route path='/' element={<Navbar />} >
+
+          <Route path='add/:activityId?' element={<EditForm />} />
+          <Route path='selectedActivity/:activityID' element={<Details />} />
+          <Route path='/act' element={<DashBoard />} />
+        </Route>
       </Routes>
-     
+
     </div>
   );
 }
