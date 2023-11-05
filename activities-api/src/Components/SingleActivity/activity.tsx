99de86e58b5c6ba76f2c7a7ea4264b803169cd27
Gele <gellehussen@gmail.com>
2023-11-05 22:59:14 +0100

    improved catagory header organzing

--- activities-api/src/Components/SingleActivity/activity.tsx
+++ activities-api/src/Components/SingleActivity/activity.tsx
@@ -4,12 +4,12 @@ import { useStore } from "../../Store/store";
 import './style.css'
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-export type Activity= {
-  id:string,
-  description:string,
-  title:string,
-  venue:string,
-  date:string
+export type Activity = {
+  id: string,
+  description: string,
+  title: string,
+  venue: string,
+  date: string
 
 }
 type Props = {
@@ -19,27 +19,33 @@ type Props = {
 
 const SingleActivity = observer(({ activity }: Props) => {
   let { id, title, date, description } = activity;
-  const {activityStore} = useStore();
+  const { activityStore } = useStore();
   const Navigate = useNavigate();
 
   return (
 
     <div key={id} className="activity-container">
-     
-     <div className="actvityHeader"> 
-      <h2 className="title">{title}</h2>
+
+      <div className="actvityHeader">
+        <div>
+          <img src={UserIcon} />
+        </div>
+        <div>
+          <h2 className="title">{title}</h2>
+          
+        </div>
       </div>
 
-      <div className="activityDetails"> 
-      <p className="date-added">{date}</p>
-    
-      <p className="description">{description}</p>
+      <div className="activityDetails">
+        <p className="date-added">{date}</p>
+
+        <p className="description">{description}</p>
       </div>
 
       <div className="actvityDescription">
-      <button onClick={()=> Navigate(`/selectedActivity/${id}`) } className="read-more-button">Read More</button>
-      <button className="deleteButton" onClick={()=> activityStore.deleteActivity(id)}>
-        Remove </button>
+        <button onClick={() => Navigate(`/selectedActivity/${id}`)} className="read-more-button">Read More</button>
+        <button className="deleteButton" onClick={() => activityStore.deleteActivity(id)}>
+          Remove </button>
       </div>
     </div>
 
